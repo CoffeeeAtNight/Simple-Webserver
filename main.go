f3a59e898353d849edd01a2e0ca459c26a08cd15
Ak1 <github@maurice-johnson.de>
2023-11-02 02:26:12 +0200

    moved start into own function

--- main.go
+++ main.go
@@ -7,13 +7,16 @@ import (
 
 var logger *log.Logger = log.Default()
 
+func startWebServer(started chan bool) {
+	if err := http.ListenAndServe(":8080", http.FileServer(http.Dir("./static"))); err != nil {
+		log.Fatal("Error occurred starting webserver")
+	}
+	started <- true
+}
+
 func main() {
 	logger.Println("Starting webserver")
-	go func() {
-		if err := http.ListenAndServe(":8080", http.FileServer(http.Dir("./static"))); err != nil {
-			log.Fatal("Error occurred starting webserver")
-		}
-	}()
-
-	select {}
+	started := make(chan bool)
+	go startWebServer(started)
+	<-started
 }
