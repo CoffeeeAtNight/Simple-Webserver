8da22d7b52ece0198838fbf1ec772261ca6bd051
Ak1 <github@maurice-johnson.de>
2023-11-02 02:19:45 +0200

    Refactoring

--- main.go
+++ main.go
@@ -7,17 +7,13 @@ import (
 
 var logger *log.Logger = log.Default()
 
-func startServer(handler http.Handler, done chan bool) {
-	logger.Println("Starting webserver")
-	if err := http.ListenAndServe(":8080", handler); err != nil {
-		log.Fatal("Error occurred starting webserver")
-	}
-	done <- true
-}
-
 func main() {
-	fileServer := http.FileServer(http.Dir("./static"))
-	done := make(chan bool)
-	go startServer(fileServer, done)
-	<-done
+	logger.Println("Starting webserver")
+	go func() {
+		if err := http.ListenAndServe(":8080", http.FileServer(http.Dir("./static"))); err != nil {
+			log.Fatal("Error occurred starting webserver")
+		}
+	}()
+
+	select {}
 }
